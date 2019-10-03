6adeec50ec2f3126eec13278ca0c91c6a77809b0
Salvador Rico <salvarico@gmail.com>
2019-10-02 23:35:18 -0500

    change pickle model

--- app/server.py
+++ app/server.py
@@ -10,9 +10,10 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+# export_file_name = 'export.pkl'
+export_file_name = 'finalized_model.pkl'
 
-classes = ['black', 'grizzly', 'teddy bears']
+classes = ['True', 'False']
 path = Path(__file__).parent
 
 app = Starlette()
@@ -30,8 +31,9 @@ async def download_file(url, dest):
 
 
 async def setup_learner():
-    await download_file(export_file_url, path / export_file_name)
+    # await download_file(export_file_url, path / export_file_name)
     try:
+        # learn = load_learner(path, export_file_name)
         learn = load_learner(path, export_file_name)
         return learn
     except RuntimeError as e:
