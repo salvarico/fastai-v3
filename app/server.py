575a8bee55f5906dd20d3b2d421eac5d5fe5f770
Salvador Rico <salvarico@gmail.com>
2019-10-04 17:33:57 -0500

    try with another model

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 # export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_url = 'https://www.dropbox.com/s/7r1r4w1nhigf5mi/finalized_model.sav?dl=1'
 # export_file_name = 'export.pkl'
-export_file_name = 'finalized_model.pkl'
+export_file_name = 'finalized_model.sav'
 
 classes = ['black', 'grizzly', 'teddys']
 # classes = ['True', 'False']
