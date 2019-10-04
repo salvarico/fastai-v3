070fc4d33ed2fc851cd37b540cef2a4796f823c2
Salvador Rico <salvarico@gmail.com>
2019-10-04 17:50:05 -0500

    try with another model

--- app/server.py
+++ app/server.py
@@ -10,9 +10,9 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 # export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/7r1r4w1nhigf5mi/finalized_model.sav?dl=1'
+export_file_url = 'https://www.dropbox.com/s/m6v89wemmdojx49/list_pickle.pkl?dl=1'
 # export_file_name = 'export.pkl'
-export_file_name = 'finalized_model.sav'
+export_file_name = 'list_pickle.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
 # classes = ['True', 'False']
