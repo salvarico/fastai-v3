c0cc7cdc694fb787fc45339d35cebeaeb93bc9cb
Salvador Rico <salvarico@gmail.com>
2019-10-03 23:42:34 -0500

    proving that the model can be changed

--- app/server.py
+++ app/server.py
@@ -10,8 +10,8 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
-# export_file_name = 'finalized_model.pkl'
+# export_file_name = 'export.pkl'
+export_file_name = 'finalized_model.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
 # classes = ['True', 'False']
