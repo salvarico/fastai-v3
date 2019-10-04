7b986a01fc9241a05fdf3bca7820bee2d3d0eefd
Salvador Rico <salvarico@gmail.com>
2019-10-04 09:11:32 -0500

    for deployment without receiving an image

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-# export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/open?id=1RnxTQkQ2AHbVpJT20Shyc6oIWbW44yby'
-# export_file_name = 'export.pkl'
-export_file_name = 'finalized_model.pkl'
+export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+# export_file_url = 'https://drive.google.com/open?id=1RnxTQkQ2AHbVpJT20Shyc6oIWbW44yby'
+export_file_name = 'export.pkl'
+# export_file_name = 'finalized_model.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
 # classes = ['True', 'False']
